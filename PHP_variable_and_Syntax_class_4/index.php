9be02c7afd52cb42d1111160301256dcc5079a48
Md. Nadim Mir <mdnadimmir150@gmail.com>
2025-01-13 03:28:08 +0600

    PHP variables and Syntax

---
+++ PHP_variable_and_Syntax_class_4/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP variable and Syntax </title>
+</head>
+<body>
+     <h1>Allah Help Me</h1>
+
+     <!-- php code  -->
+
+     <?php 
+
+        echo 'PHP variable and Syntax ';
+        echo "<br>";
+        echo nl2br("PHP variable and Syntax2 \n PHP variable and Syntax3 \n  PHP variable and Syntax4");
+
+        // echo "<br>";
+        // echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
+
+
+        echo "</br>";
+        echo "</br>";
+
+        echo "I love Hazrat Muhammad Sallahu Alihi Oasallam";
+        echo "</br>";
+
+        echo "I love  all poygomber ";
+        echo "</br>";
+
+
+        echo nl2br("I love AL Quran \n I love Honesty \n I love Politeness");
+       
+     
+     ?>
+
+</body>
+</html>
