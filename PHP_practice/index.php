43970a672ea564d10715956d78cf3b8d43f07240
Md. Nadim Mir <mdnadimmir150@gmail.com>
2025-01-13 11:50:41 +0600

    data type php

--- PHP_practice/index.php
+++ PHP_practice/index.php
@@ -23,14 +23,14 @@
         // echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
 
 
-        echo "</br>";
-        echo "</br>";
+        echo "<br>";
+        echo "<br>";
 
         echo "I love Hazrat Muhammad Sallahu Alihi Oasallam";
-        echo "</br>";
+        echo "<br>";
 
         echo "I love  all poygomber ";
-        echo "</br>";
+        echo "<br>";
 
 
         echo nl2br("I love AL Quran \n I love Honesty \n I love Politeness");
@@ -41,9 +41,9 @@
         ## Topic : Dynamic variable create
 
 
-        echo "</br>";
-        echo "</br>";
-        echo "</br>";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
 
         $age =24;
         $name= "Mohammad Nadim Mir";
@@ -51,17 +51,17 @@
         // dynamic variable added into double quoute
         echo "My name is  $name , Age is  $age ";
 
-        echo "</br>";
-        echo "</br>";
-        echo "</br>";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
 
         // double quoute . use 
         echo "Name is ".$name." , Age  ".$age;
 
 
-        echo "</br>";
-        echo "</br>";
-        echo "</br>";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
 
         // single quoute . use 
         echo 'Name is '.$name.' , Age  '.$age;
@@ -69,8 +69,8 @@
 
         // double dollar work 
 
-        echo ('</br>');
-        echo ('</br>');
+        echo ('<br>');
+        echo ('<br>');
 
         $nickname = "adnan";
         $name ="nickname";
@@ -84,7 +84,7 @@
 
         ## Topic : comment needed or use
 
-        echo ('</br>');
+        echo ('<br>');
 
         $a= 5 ;
         $b=17;
@@ -94,7 +94,7 @@
 
         echo $a+$b; 
 
-        echo ('</br>');
+        echo ('<br>');
         echo $a+$b+$c;
 
         /*
@@ -102,35 +102,51 @@
            comment
         */
 
-        echo ('</br>');
-        echo /*$a+*/$b+$c.("</br>").("</br>").("</br>");  // multi line comment
+        echo ('<br>');
+        echo /*$a+*/$b+$c.("<br>").("<br>").("<br>");  // multi line comment
 
 
 
         ## Topic : PHP  Constant variable use 
 
         define("Car","BMW");
-        echo Car.("</br>");  // constant name call
+        echo Car.("<br>");  // constant name call
 
 
-        echo constant("Car").("</br>");
+        echo constant("Car").("<br>");
 
         $model="constant";
-        echo $model("Car").("</br>").("</br>");
+        echo $model("Car").("<br>").("<br>");
         
 
         ## Topic : Echo and Print use
 
         $j = "I love Bangladesh";
-        print $j.("</br>").("</br>");
+        print $j.("<br>").("<br>");
 
         $k = print $j;
 
-        echo $k.("</br>").("</br>");
-        echo $k.("</br>").("</br>");
+        echo $k.("<br>").("<br>");
+        echo $k.("<br>").("<br>");
+
+
+        # Topic : PHP data types 
+ 
+        $biscuit = 2 ; // int type variable
+        $cost = 3.4 ; // float type variable
+        $chocolate = 'dary milk' ; // string type variable
+        $status = true ;  // boolean type variable true = 1 , false = ''
+        $data = null;
+
+        echo $biscuit."<br>" , $cost."<br>" , $chocolate."<br>" , $status."<br>" , $data."<br>" ;
+
+        $status = false ; // '' value
+        $data = "info";
+
+        echo "hello" , $status.("<br>") , $data.("<br>");
+
 
 
     ?>
-        
 </body>
 </html>
